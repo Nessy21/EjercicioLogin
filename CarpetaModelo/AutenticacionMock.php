aaa5f7d14418a7d2fc0c46be46cc3461544969e9
Nessy <vaneee10@gmail.com>
2020-10-09 11:55:18 +0200

    .

--- CarpetaModelo/AutenticacionMock.php
+++ CarpetaModelo/AutenticacionMock.php
@@ -1,6 +1,17 @@
 <?php
+    include "../CarpetaVista/autenticar.php";
 
+    $usuario = array (
+        1=> "nessy",
+        2=> "admin"
+    );
 
+    $pwd = array (
+        1=>"1234",
+        2=>"admin"
+    );
 
+    function autenticarUsuario ($){
 
+    }
 ?>
--- CarpetaModelo/Usuario.php
+++ CarpetaModelo/Usuario.php
@@ -1,6 +1,45 @@
 <?php
+        
+    class Usuario {
+        //atributos 
+        private $nombre;
+        private $apellidos;
+        private $login;
+        private $password;
 
+        //getters y setters
+        public function getNombre(){
+            return $this->nombre;
+        }
+        public function setNombre($nombre){
+            $this->nombre=$nombre;
+        }
+        public function getApellidos(){
+            return $this->apellidos;
+        }
+        public function setApellidos($apellidos){
+            $this->apellidos=$apellidos;
+        }
+        public function getLogin(){
+            return $this->login;
+        }
+        public function setLogin($login){
+            $this->login=$login;
+        }
+        public function getPassword(){
+            return $this->password;
+        }
+        public function setPassword($password){
+            $this->password=$password;
+        }
 
+        //constructor
 
-
+        public function __construct($nombre, $apellidos, $login, $password){
+            $this->nombre=$nombre;
+            $this->apellidos=$apellidos;
+            $this->login=$login;
+            $this->password=$password;
+        }
+    }    
 ?>
