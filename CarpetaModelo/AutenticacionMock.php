3cc20fe231f3630bb578f9a1a7b50345eb4f0cb8
Nessy <vaneee10@gmail.com>
2020-10-19 08:49:23 +0200

    .

--- CarpetaModelo/AutenticacionMock.php
+++ CarpetaModelo/AutenticacionMock.php
@@ -1,17 +1,30 @@
 <?php
-    include "../CarpetaVista/autenticar.php";
+    include_once "../CarpetaVista/autenticar.php";
+    include_once "Usuario.php";
+    class AutenticacionMock{
+        
 
-    $usuario = array (
-        1=> "nessy",
-        2=> "admin"
-    );
-
-    $pwd = array (
-        1=>"1234",
-        2=>"admin"
-    );
-
-    function autenticarUsuario ($){
-
+        function autenticarUsuario ($user, $password){
+            //creacion de objetos
+            $u1 = new Usuario ("Flor", "Pérez", "flo", "123" );
+            $u2 = new Usuario ("Carlos", "Salas", "cali", "1234");
+            
+            //array de objetos
+            $a = array (
+                1=> $u1,
+                2=> $u2
+            );
+            //invocar un getlogin y getpassword
+            foreach($a as $usu){
+                //si es correcto devuelve tipo user, sino un false
+                if(strcmp($user, $usu->getLogin())==0){//mirar si coincide el usuario
+                   //comprobar aqui las contraseñas
+                        if(strcmp($password, $usu->getPassword())==0){
+                            return $usu;
+                        }
+                    }
+                }//final de comprobacion
+            return false; 
+        }//Autenticar usuario
     }
 ?>
--- CarpetaVista/autenticar.php
+++ CarpetaVista/autenticar.php
@@ -1,9 +1,19 @@
 <?php
-    $user=$_POST["user"];
-    $pwd=$_POST["pwd"];
 
+    include_once "../CarpetaModelo/AutenticacionMock.php";
+    $user = $_POST['user'];
+    $paswsword = $_POST['password'];
 
+    //llamamos a la funcion autenticar usuario//
 
+    $mc = new AutenticacionMock();
+    $mc-> autenticarUsuario($user, $pwd);//para invocar se utiliza ->
+    $resultado=$mc-> autenticarUsuario($user, $pwd); //para guardar el resultado
 
-    //
+    //Si no es falso-> objeto 
+    if ($resultado!=false){
+        header ("Location: datosUsuario.php?user=$user&password=$password");//redireccionar
+    }else{
+        header ("Location: errorAutenticacion.php");
+    }
 ?>
